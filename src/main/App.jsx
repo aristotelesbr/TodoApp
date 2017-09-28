3ce0d01cf03e8b63d015d7dfe92a1dfaa36c2464
Aristóteles Coutinho <aristotelesbr2014@gmail.com>
2017-09-28 16:44:07 -0300

    Adding bootstrap, font-awesome, axios and TodoComponent and AboutComponent

---
+++ src/main/App.jsx
@@ -0,0 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import 'font-awesome/css/font-awesome.min.css';
+
+import React from 'react'
+import Todo from '../todo/Todo'
+import About from '../about/About'
+
+export default props => (
+	<div className='container'>
+		<Todo />
+		<About />
+	</div>
+)
