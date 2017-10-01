24ccf60f3224447464f568d50990d1fb2239ec4f
Aristóteles Coutinho <aristotelesbr2014@gmail.com>
2017-10-01 10:17:17 -0300

    Adding IfComponent

--- src/template/IconButton.js
+++ src/template/IconButton.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
+import If from './If';
 
 export default class IconButton extends Component {
 	render() {
-		if (this.props.hide) {
-			return null
-		} else {
-			return (
+		return(
+			<If test={!this.props.hide}>
 				<button className={'btn btn-'+ this.props.style} onClick={this.props.onClick}>
 					<i className={'fa fa-'+ this.props.icon}></i>
 				</button>
-			)
-		}
+			</If>
+		)
 	}
 }
+
---
+++ src/template/If.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export default class If extends Component {
+	render() {
+		if(this.props.test) {
+			return this.props.children
+		} else {
+			return false
+		}
+	}
+}
