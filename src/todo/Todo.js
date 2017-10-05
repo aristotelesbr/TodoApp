4fcd4b03d642ed68636a632d23fbfcd5be7c9741
Aristóteles Coutinho <aristotelesbr2014@gmail.com>
2017-10-05 10:16:42 -0300

    integration with backend

--- src/todo/Todo.js
+++ src/todo/Todo.js
@@ -1,15 +1,59 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
 import PageHeader from '../template/PageHeader';
 import TodoForm from './TodoForm';
 import TodoList from './TodoList';
 
+
+const URL = 'http://localhost:3003/api/todos'
+
 export default class Todo extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			description: '', 
+			list: []
+		}
+
+		this.handleRemove = this.handleRemove.bind(this)
+		this.handleChange = this.handleChange.bind(this)	
+		this.handleAdd = this.handleAdd.bind(this)
+
+		this.refresh()
+	}
+
+	refresh() {
+		axios.get(`${URL}?sort=-createdAt`)
+			.then(resp => this.setState({...this.state, description: '', list: resp.data}))
+	}
+
+	handleRemove(todo) {
+		axios.delete(`${URL}/${todo._id}`)
+			.then(resp => this.refresh())
+	}
+
+	handleChange(e) {
+		this.setState({...this.state, description: e.target.value})
+	}
+
+	handleAdd() {
+		const description = this.state.description
+		axios.post(URL, { description })
+			.then(resp => this.refresh())
+	}
+
 	render() {
 		return (
 			<div>
 				<PageHeader name='Tarefas' small='Cadastro'></PageHeader>
-				<TodoForm />
-				<TodoList />
+				<TodoForm 
+					description={this.state.description}
+					handleChange={this.handleChange}
+					handleAdd={this.handleAdd} />
+				<TodoList 
+					list={this.state.list}
+					handleRemove={this.handleRemove}/>
 			</div>
 		)
 	}
--- src/todo/TodoList.js
+++ src/todo/TodoList.js
@@ -1,9 +1,32 @@
 import React, {Component} from 'react';
+import IconButton from '../template/IconButton';
 
 export default class TodoList extends Component {
 	render() {
+
+		const renderRows = () => {
+			const list = this.props.list || []
+			return list.map(todo => (
+				<tr key={todo._id}>
+					<td>{todo.description}</td>
+					<td>
+						<IconButton style='danger' icon='trash-o' onClick={() => this.props.handleRemove(todo)} />
+					</td>
+				</tr>
+			))
+		}
+
 		return(
-			<div>Listagem de Tarefas</div>
+			<table className='table'>
+				<thead>
+					<tr>
+						<td>Descrição</td>
+					</tr>
+				</thead>
+				<tbody>
+					{renderRows()}
+				</tbody>
+			</table>
 		)
 	}
 }
