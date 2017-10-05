6c02d54b7e9ca6daf5d302a2448fb033126fce00
Aristóteles Coutinho <aristotelesbr2014@gmail.com>
2017-10-05 13:44:32 -0300

    Adding keyboard shortcuts

--- src/todo/TodoForm.js
+++ src/todo/TodoForm.js
@@ -5,6 +5,14 @@ import IconButton from '../template/IconButton';
 
 export default class TodoForm extends Component {
 	render() {
+		const keyHandler = (e) => {
+			if (e.key === 'Enter') {
+				e.shiftKey ? this.props.handleSearch() : this.props.handleAdd()
+			} else if (e.key === 'Escape'){
+				this.props.handleClear();
+			}
+		}
+
 		return(
 			<div role='form' className='todoForm'>
 				<Grid cols='12 9 10'>
@@ -12,6 +20,7 @@ export default class TodoForm extends Component {
 						id='description' 
 						className='form-control' 
 						placeholder='Adicione uma tarefa'
+						onKeyUp={keyHandler}
 						onChange={this.props.handleChange}
 						value={this.props.description}>
 					</input>
