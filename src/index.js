e2e5785ab22211154f1a696ca2db914db0f2fc8b
Aristóteles Coutinho <aristotelesbr2014@gmail.com>
2017-10-06 09:13:43 -0300

    Redux settings

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+// Redux components for instegration
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
 import App from './main/App';
 import registerServiceWorker from './registerServiceWorker';
+// import static reducers
+import reducers from './main/reducers';
 
+// create a store
+const store = createStore(reducers)
+
+// usage store in top element of components
 ReactDOM.render(
 	<BrowserRouter>
-		<App />
+		<Provider store={store}>
+			<App />
+		</Provider>
 	</BrowserRouter>, document.getElementById('root')
 );
 registerServiceWorker();
